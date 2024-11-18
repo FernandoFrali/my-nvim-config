d48ef1f571bde335ae780f339b3891922f6d2069
Fernando Oliveira <112976898+FernandoFrali@users.noreply.github.com>
2024-11-18 15:26:41 -0300

    Reapply ":wrench: config: change nerdcommenter keybinds to dont conflict with code actions command"
    This reverts commit c589ce22833c6654167170aabfa2a9bb27ffb7f2.

--- lua/fernandofrali/remap.lua
+++ lua/fernandofrali/remap.lua
@@ -1,3 +1,9 @@
+-- Nerd Commenter
+vim.g.NERDCreateDefaultMappings = 0
+
+vim.keymap.set("n", "<leader>cc", "<Plug>NERDCommenterToggle")
+--
+
 vim.g.mapleader = " "
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -44,7 +50,7 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
+vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.config/nvim/lua/fernandofrali/packer.lua<CR>");
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
 vim.keymap.set("n", "<leader><leader>", function()
